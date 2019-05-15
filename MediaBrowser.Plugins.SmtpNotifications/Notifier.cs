3d47370f7fd019d588eb92ee7899792ae416b0d2
Bond-009 <bond.009@outlook.com>
2019-05-15 13:32:09 +0200

    Fix build errors

--- MediaBrowser.Plugins.SmtpNotifications/Notifier.cs
+++ MediaBrowser.Plugins.SmtpNotifications/Notifier.cs
@@ -1,27 +1,23 @@
-﻿using MediaBrowser.Controller.Entities;
-using MediaBrowser.Controller.Notifications;
-using MediaBrowser.Controller.Security;
-using Microsoft.Extensions.Logging;
-using MediaBrowser.Plugins.SmtpNotifications.Configuration;
-using System;
+﻿using System;
 using System.Linq;
 using System.Net;
 using System.Net.Mail;
 using System.Threading;
 using System.Threading.Tasks;
+using MediaBrowser.Controller.Entities;
+using MediaBrowser.Controller.Notifications;
+using MediaBrowser.Plugins.SmtpNotifications.Configuration;
+using Microsoft.Extensions.Logging;
 
 namespace MediaBrowser.Plugins.SmtpNotifications
 {
     public class Notifier : INotificationService
     {
         private readonly ILogger _logger;
-        public static Notifier Instance { get; private set; }
 
         public Notifier(ILogger logger)
         {
             _logger = logger;
-
-            Instance = this;
         }
 
         public bool IsEnabledForUser(User user)
@@ -32,35 +28,12 @@ namespace MediaBrowser.Plugins.SmtpNotifications
         }
 
         private SMTPOptions GetOptions(User user)
-        {
-            return Plugin.Instance.Configuration.Options
+            => Plugin.Instance.Configuration.Options
                 .FirstOrDefault(i => string.Equals(i.UserId, user.Id.ToString("N"), StringComparison.OrdinalIgnoreCase));
-        }
 
-        public string Name
-        {
-            get { return Plugin.Instance.Name; }
-        }
+        public string Name => Plugin.Instance.Name;
 
-
-        public Task SendNotification(UserNotification request, CancellationToken cancellationToken)
-        {
-            return Task.Run(() => TrySendNotification(request, cancellationToken));
-        }
-
-        private void TrySendNotification(UserNotification request, CancellationToken cancellationToken)
-        {
-            try
-            {
-                SendNotificationCore(request, cancellationToken);
-            }
-            catch (Exception ex)
-            {
-                _logger.LogError(ex, "Error sending email");
-            }
-        }
-
-        private void SendNotificationCore(UserNotification request, CancellationToken cancellationToken)
+        public async Task SendNotification(UserNotification request, CancellationToken cancellationToken)
         {
             var options = GetOptions(request.User);
 
@@ -69,38 +42,43 @@ namespace MediaBrowser.Plugins.SmtpNotifications
                 Subject = "Emby: " + request.Name,
                 Body = string.Format("{0}\n\n{1}", request.Name, request.Description)
             })
+            using (var client = new SmtpClient
             {
-                using (var client = new SmtpClient
+                Host = options.Server,
+                Port = options.Port,
+                DeliveryMethod = SmtpDeliveryMethod.Network,
+                UseDefaultCredentials = false,
+                Timeout = 20000
+            })
+            {
+                if (options.SSL)
                 {
-                    Host = options.Server,
-                    Port = options.Port,
-                    DeliveryMethod = SmtpDeliveryMethod.Network,
-                    UseDefaultCredentials = false,
-                    Timeout = 20000
-                })
+                    client.EnableSsl = true;
+                }
+
+                _logger.LogInformation("Sending email {to} with subject {subject}", options.EmailTo, mail.Subject);
+
+                if (options.UseCredentials
+                    && !string.IsNullOrEmpty(options.Username)
+                    && !string.IsNullOrEmpty(options.Password))
                 {
-                    if (options.SSL) client.EnableSsl = true;
+                    client.Credentials = new NetworkCredential(options.Username, options.Password);
+                }
+                else
+                {
+                    _logger.LogError(
+                        "Cannot use credentials for email to {User} because the username or password is missing",
+                        options.EmailTo);
+                }
 
-                    _logger.LogInformation("Sending email {to} with subject {subject}", options.EmailTo, mail.Subject);
-
-                    if (options.UseCredentials && !string.IsNullOrEmpty(options.Username) && !string.IsNullOrEmpty(options.Password))
-                    {
-                        client.Credentials = new NetworkCredential(options.Username, options.Password);
-                    }
-                    else
-                    {
-                        _logger.LogError("Cannot use credentials for email to {user} because the username or password is missing", options.EmailTo);
-                    }
-
-                    try
-                    {
-                        client.Send(mail);
-                        _logger.LogInformation("Completed sending email {to} with subject {subject}", options.EmailTo, mail.Subject);
-                    }
-                    catch (Exception ex)
-                    {
-                        _logger.LogError(ex, "Error sending email");
-                    }
+                try
+                {
+                    await client.SendMailAsync(mail).ConfigureAwait(false);
+                    _logger.LogInformation("Completed sending email {to} with subject {subject}", options.EmailTo, mail.Subject);
+                }
+                catch (Exception ex)
+                {
+                    _logger.LogError(ex, "Error sending email");
                 }
             }
         }
